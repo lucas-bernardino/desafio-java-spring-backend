849d3e58f7638c1581fddda3ae998f82d21e360b
lucas-bernardino <lucasgber@gmail.com>
2024-12-31 10:27:26 -0300

    Initial commit

---
+++ src/test/java/com/bernardino/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardino.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
